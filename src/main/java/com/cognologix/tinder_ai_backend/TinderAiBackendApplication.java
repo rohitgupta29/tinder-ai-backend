0b385c95a7b39a8f4fe5299ef908b9c12556186a
rohitgupta <rohit.gupta@cognologix.com>
2024-11-26 12:20:32 +0530

    Add a profile to db and fetch it successfully

--- src/main/java/com/cognologix/tinder_ai_backend/TinderAiBackendApplication.java
+++ src/main/java/com/cognologix/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,13 +1,38 @@
 package com.cognologix.tinder_ai_backend;
 
+import com.cognologix.tinder_ai_backend.profiles.Gender;
+import com.cognologix.tinder_ai_backend.profiles.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.cognologix.tinder_ai_backend.profiles.Profile;
 
 @SpringBootApplication
 public class TinderAiBackendApplication {
 
+	@Autowired
+	private ProfileRepository profileRepository;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(TinderAiBackendApplication.class, args);
 	}
 
+	public void run(String... args) {
+		Profile profile = new Profile(
+				"1",
+				"Rohit",
+				"Gupta",
+				34,
+				"Indian",
+				Gender.MALE,
+				"Software Programmer",
+				 "foo.img",
+				"INTP"
+		);
+		profileRepository.save(profile);
+		System.out.println("My app is running");
+		profileRepository.findAll().forEach(System.out::println);
+//
+	}
 }
---
+++ src/main/java/com/cognologix/tinder_ai_backend/profiles/Profile.java
@@ -0,0 +1,15 @@
+package com.cognologix.tinder_ai_backend.profiles;
+
+
+public record Profile(
+                       String id,
+                       String firstName,
+                       String lastName,
+                       int age,
+                       String ethnicity,
+                       Gender gender,
+                       String bio,
+                       String imageUrl,
+                       String myersBriggsPersonalityType) {
+
+}
